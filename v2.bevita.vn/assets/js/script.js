20848489a7ac742187f79ad59bc24fd6a8b32b12
Nguyễn Thanh Hào <67219502+nguyenthanhhao18012000@users.noreply.github.com>
2025-11-29 16:02:58 +0700

    details service

--- v2.bevita.vn/assets/js/script.js
+++ v2.bevita.vn/assets/js/script.js
@@ -4,7 +4,9 @@ jQuery(function ($) {
         'i-custom_arrow-right': 'fa-arrow-right',
         'i-custom_calendar-alt': 'fa-calendar-alt',
         'i-custom_phone': 'fa-phone',
-        'i-custom_comment': 'fa-comment'
+        'i-custom_comment': 'fa-comment',
+        'i-custom_trophy': 'fa-trophy',
+        'i-custom_shield-virus': 'fa-shield-virus'
     };
 
     $.each(iconMap, function (customClass, faIcon) {
@@ -34,4 +36,45 @@ jQuery(document).ready(function ($) {
         wrapAround: false,
         // freeScroll: true
     });
+
+
+    jQuery(function ($) {
+        $('._cards-box').each(function () {
+            const $box = $(this);
+            const $imageNodes = $box.find('._card:not(._hide)');
+            const arrIndexes = [];
+
+            for (let i = 0; i < $imageNodes.length; i++) {
+                arrIndexes.push(i);
+            }
+
+            function setIndex(arr) {
+                $imageNodes.each(function (i) {
+                    $(this).attr('data-slide', arr[i]);
+                });
+            }
+
+            function nextSlide() {
+                arrIndexes.push(arrIndexes.shift());
+                setIndex(arrIndexes);
+            }
+
+            $box.on('click', function () {
+                nextSlide();
+            });
+
+            let autoplay = setInterval(nextSlide, 3000);
+
+            $box.on('mouseenter', function () {
+                clearInterval(autoplay);
+            });
+
+            $box.on('mouseleave', function () {
+                autoplay = setInterval(nextSlide, 3000);
+            });
+
+            setIndex(arrIndexes);
+        });
+    });
+
 });
