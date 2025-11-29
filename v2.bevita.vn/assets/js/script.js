62bb7c7c4d9b7cea4a56fc5343a13cfef14c3040
Nguyễn Thanh Hào <67219502+nguyenthanhhao18012000@users.noreply.github.com>
2025-11-29 13:46:33 +0700

    push css

--- v2.bevita.vn/assets/js/script.js
+++ v2.bevita.vn/assets/js/script.js
@@ -1,17 +1,24 @@
 jQuery(function ($) {
-    $('.i-custom_arrow-left').each(function () {
-        $(this).children('i').first().replaceWith('<i class="fas fa-arrow-left"></i>');
-        $(this).addClass('has-changed');
-    });
-    $('.i-custom_arrow-right').each(function () {
-        $(this).children('i').first().replaceWith('<i class="fas fa-arrow-right"></i>');
-        $(this).addClass('has-changed');
+    const iconMap = {
+        'i-custom_arrow-left': 'fa-arrow-left',
+        'i-custom_arrow-right': 'fa-arrow-right',
+        'i-custom_calendar-alt': 'fa-calendar-alt',
+        'i-custom_phone': 'fa-phone',
+        'i-custom_comment': 'fa-comment'
+    };
+
+    $.each(iconMap, function (customClass, faIcon) {
+        $('.' + customClass).each(function () {
+            $(this).children('i').first().replaceWith('<i class="fas ' + faIcon + '"></i>');
+            $(this).addClass('has-changed');
+        });
     });
 });
 
 
+
 jQuery(document).ready(function ($) {
-    jQuery('.row-testimonial-slider').flickity({
+    jQuery('.row-slider-3').flickity({
         cellAlign: 'left',
         contain: true,
         pageDots: false,
@@ -19,5 +26,12 @@ jQuery(document).ready(function ($) {
         wrapAround: false,
         // freeScroll: true
     });
-
+    jQuery('.row-slider-2').flickity({
+        cellAlign: 'left',
+        contain: true,
+        pageDots: false,
+        groupCells: 2,
+        wrapAround: false,
+        // freeScroll: true
+    });
 });
